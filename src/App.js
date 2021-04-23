b84bac1e089a98be6386c4c1310ba268b93ff852
dhchun1203 <dhchun1203@gmail.com>
2021-04-23 15:43:55 +0900

    ScrollToTop button, Title link to google, CSS modification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import React, { Component } from "react";
 import Movie from "./Movie";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
 	state = {
 		isLoading: true,
 		movies: [],
+		theposition: Object,
 	};
 	getMovies = async () => {
 		const {
@@ -20,15 +21,48 @@ class App extends Component {
 			isLoading: false,
 			movies,
 		});
-		console.log(movies);
 	};
 	componentDidMount() {
 		this.getMovies();
+		window.addEventListener("scroll", this.listenToScroll);
 	}
+	listenToScroll = () => {
+		const winScroll =
+			document.body.scrollTop || document.documentElement.scrollTop;
+
+		const height =
+			document.documentElement.scrollHeight -
+			document.documentElement.clientHeight;
+
+		const scrolled = winScroll / height;
+
+		this.setState({
+			theposition: scrolled,
+		});
+		this.handleScroll(this.state.theposition);
+	};
+	handleScroll(theposition) {
+		if (theposition >= 0.1) {
+			document.querySelector(".scrollBtn").classList.add("visible");
+		} else {
+			document.querySelector(".scrollBtn").classList.remove("visible");
+		}
+	}
+	scrollToTop = () => {
+		window.scroll({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
 	render() {
 		const { isLoading, movies } = this.state;
 		return (
 			<section className="container">
+				<button className="scrollBtn" onClick={this.scrollToTop}>
+					<span>
+						<i className="fas fa-angle-double-up"></i>
+					</span>
+				</button>
 				{isLoading ? (
 					<div className="loader">
 						<span className="loader__text">Loading...</span>
--- src/Movie.js
+++ src/Movie.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 import "./Movie.scss";
 
 function Movie({ year, title, summary, poster, genres, rating }) {
-	console.log(genres);
-	console.log(rating);
+	const titleUrl = `http://www.google.com/search?q=${title}`;
 	return (
 		<div className="movie">
 			<img src={poster} alt="poster" title={title} />
 			<div className="movie__data">
-				<h3 className="movie__title">{title}</h3>
+				<a href={titleUrl} target="_blank">
+					<span>
+						<h3 className="movie__title">{title}</h3>
+					</span>
+				</a>
 				<h5 className="movie__year">{year}</h5>
 				<p className="movie__genres">{genres.join(", ")}</p>
 				<p className="movie__rating">Rating: {rating}/10</p>
